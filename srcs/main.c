a2df762c540c9b583beb4bcb0ccb11357ce67cdd
Umut Enes Soğukpınar <usogukpinar0@gmail.com>
2025-06-04 21:34:53 +0300

    Update main.c

--- srcs/main.c
+++ srcs/main.c
@@ -4,7 +4,7 @@ int main(int argc, char **argv)
 {
     if (argc != 2) 
     {
-        ft_putendl_fd("Usage: ./so_long <map_file.cub>", STDERR_FILENO);
+        ft_putendl_fd("Usage: ./cub3d <map_file.cub>", STDERR_FILENO);
         return (EXIT_FAILURE);
     }
     if (!has_valid_cub_extension(argv[1])) 
