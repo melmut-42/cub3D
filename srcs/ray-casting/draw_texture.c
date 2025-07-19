d136da4132e54b199e03073614cb822cf73ad565
umutsogukpinar <usogukpinar0@gmail.com>
2025-07-19 21:33:31 +0300

    chore: simplified draw functions for norm rules

--- srcs/ray-casting/draw_texture.c
+++ srcs/ray-casting/draw_texture.c
@@ -1,128 +1,119 @@
 #include "game.h"
 
-static t_img	*get_wall_texture(t_game *g, t_ray *ray);
-static void		draw_texture_line(t_game *g, t_column *d);
-static int		get_texture_x(t_game *g, t_ray *ray, t_img *tex);
-
-void	draw_column(t_game *g, t_ray *ray, int x)
+t_img  *get_wall_texture(t_game *g, t_ray *ray)
 {
-	t_column	c;
-	int			ceiling_color;
-	int			floor_color;
-	int			y;
-	int			pitch;
-
-	c.wall_height = (int)(g->mlx->height / ray->perp_wall_dist);
-
-	// ! Apply vertical pitch offset
-	pitch = (int)(g->player.pitch_angle);
-	c.pixel_top = (g->mlx->height - c.wall_height) / 2 + pitch;
-	if (c.pixel_top < 0)
-		c.pixel_top = 0;
-	c.pixel_bottom = (g->mlx->height + c.wall_height) / 2 + pitch;
-	if (c.pixel_bottom >= g->mlx->height)
-		c.pixel_bottom = g->mlx->height - 1;
-
-	c.texture = get_wall_texture(g, ray);
-	c.texture_x = get_texture_x(g, ray, c.texture);
-	c.screen_x = x;
-
-	ceiling_color = rgb_to_int(
-		g->data.texture.ceil_rgb[0],
-		g->data.texture.ceil_rgb[1],
-		g->data.texture.ceil_rgb[2]);
-	floor_color = rgb_to_int(
-		g->data.texture.floor_rgb[0],
-		g->data.texture.floor_rgb[1],
-		g->data.texture.floor_rgb[2]);
-
-	y = 0;
-	while (y < c.pixel_top)
-		ft_put_pixel(&g->mlx->frame_img, x, y++, ceiling_color);
-
-	draw_texture(g, ray, x);
-
-	y = c.pixel_bottom + 1;
-	while (y < g->mlx->height)
-		ft_put_pixel(&g->mlx->frame_img, x, y++, floor_color);
+    if (ray->side == 0 && ray->dir.x > 0)
+        return (&g->data.texture.textures[EAST]);
+    if (ray->side == 0 && ray->dir.x < 0)
+        return (&g->data.texture.textures[WEST]);
+    if (ray->side == 1 && ray->dir.y > 0)
+        return (&g->data.texture.textures[SOUTH]);
+    return (&g->data.texture.textures[NORTH]);
 }
 
-void	draw_texture(t_game *g, t_ray *ray, int x)
+int     get_texture_x(t_game *g, t_ray *ray, t_img *tex)
 {
-	t_column	col;
-	int			pitch;
+    double wall_x;
+    int    tex_x;
 
-	col.wall_height = (int)(g->mlx->height / ray->perp_wall_dist);
-
-	// ! Apply vertical pitch offset
-	pitch = (int)(g->player.pitch_angle);
-	col.pixel_bottom = (g->mlx->height - col.wall_height) / 2 + pitch;
-	if (col.pixel_bottom < 0)
-		col.pixel_bottom = 0;
-	col.pixel_top = (g->mlx->height + col.wall_height) / 2 + pitch;
-	if (col.pixel_top >= g->mlx->height)
-		col.pixel_top = g->mlx->height - 1;
-
-	col.texture = get_wall_texture(g, ray);
-	col.texture_x = get_texture_x(g, ray, col.texture);
-	col.screen_x = x;
-
-	draw_texture_line(g, &col);
+    if (ray->side == 0)
+        wall_x = g->player.pos.y + ray->perp_wall_dist * ray->dir.y;
+    else
+        wall_x = g->player.pos.x + ray->perp_wall_dist * ray->dir.x;
+    wall_x -= floor(wall_x);
+    tex_x = (int)(wall_x * (double)tex->width);
+    if ((ray->side == 0 && ray->dir.x < 0)
+     || (ray->side == 1 && ray->dir.y > 0))
+        tex_x = tex->width - tex_x - 1;
+    return (tex_x);
 }
 
-static t_img	*get_wall_texture(t_game *g, t_ray *ray)
+
+static void init_column(t_column *col, t_game *g, t_ray *ray, int x)
 {
-	if (ray->side == 0 && ray->dir.x > 0)
-		return (&g->data.texture.textures[EAST]);
-	if (ray->side == 0 && ray->dir.x < 0)
-		return (&g->data.texture.textures[WEST]);
-	if (ray->side == 1 && ray->dir.y > 0)
-		return (&g->data.texture.textures[SOUTH]);
-	return (&g->data.texture.textures[NORTH]);
-}
+    int win_h;
+    int pitch;
 
-static int	get_texture_x(t_game *g, t_ray *ray, t_img *tex)
-{
-	double	wall_x;
-	int		tex_x;
-
-	if (ray->side == 0)
-		wall_x = g->player.pos.y + ray->perp_wall_dist * ray->dir.y;
-	else
-		wall_x = g->player.pos.x + ray->perp_wall_dist * ray->dir.x;
-	wall_x -= floor(wall_x);
-	tex_x = (int)(wall_x * (double)tex->width);
-	if ((ray->side == 0 && ray->dir.x < 0) || (ray->side == 1 && ray->dir.y > 0))
-		tex_x = tex->width - tex_x - 1;
-	return (tex_x);
-}
-
-static void	draw_texture_line(t_game *g, t_column *d)
-{
-	int		y;
-	double	step;
-	double	tex_pos;
-	int		tex_y;
-	int		color;
-	int		center;
-
-	step = 1.0 * d->texture->height / d->wall_height;
-
+    win_h = g->mlx->height;
+    col->wall_height = (int)(win_h / ray->perp_wall_dist);
 	// ! Use pitch-adjusted screen center
-	center = g->mlx->height / 2 + (int)(g->player.pitch_angle);
-	tex_pos = (d->pixel_bottom - center + d->wall_height / 2) * step;
+    pitch = (int)g->player.pitch_angle;
+    col->pixel_top = (win_h - col->wall_height) / 2 + pitch;
+    if (col->pixel_top < 0)
+        col->pixel_top = 0;
+    col->pixel_bottom = (win_h + col->wall_height) / 2 + pitch;
+    if (col->pixel_bottom >= win_h)
+        col->pixel_bottom = win_h - 1;
+    col->texture = get_wall_texture(g, ray);
+    col->texture_x = get_texture_x(g, ray, col->texture);
+    col->screen_x = x;
+}
 
-	y = d->pixel_bottom;
-	while (y <= d->pixel_top)
-	{
-		if (tex_pos >= 0 && tex_pos < d->texture->height)
-		{
-			tex_y = (int)tex_pos;
-			color = *(unsigned int *)(d->texture->addr
-					+ (tex_y * d->texture->line_len + d->texture_x * (d->texture->bpp / 8)));
-			ft_put_pixel(&g->mlx->frame_img, d->screen_x, y, color);
-		}
-		tex_pos += step;
-		y++;
-	}
+static void draw_ceiling(t_game *g, t_column *col, int color)
+{
+    int y;
+
+    y = 0;
+    while (y < col->pixel_top)
+        ft_put_pixel(&g->mlx->frame_img, col->screen_x, y++, color);
+}
+
+static void draw_floor(t_game *g, t_column *col, int color)
+{
+    int y;
+    int win_h;
+
+    win_h = g->mlx->height;
+    y = col->pixel_bottom + 1;
+    while (y < win_h)
+        ft_put_pixel(&g->mlx->frame_img, col->screen_x, y++, color);
+}
+
+static void draw_wall(t_game *g, t_column *col)
+{
+    int     y;
+    double  step;
+    double  tex_pos;
+    int     tex_y;
+    int     color;
+
+    step = (double)col->texture->height / col->wall_height;
+    tex_pos = (col->pixel_top
+               - (g->mlx->height / 2 + (int)g->player.pitch_angle)
+               + col->wall_height / 2)
+              * step;
+    y = col->pixel_top;
+    while (y <= col->pixel_bottom)
+    {
+        if (tex_pos >= 0 && tex_pos < col->texture->height)
+        {
+            tex_y = (int)tex_pos;
+            color = *(unsigned int *)(col->texture->addr
+                + tex_y * col->texture->line_len
+                + col->texture_x * (col->texture->bpp / 8));
+            ft_put_pixel(&g->mlx->frame_img, col->screen_x, y, color);
+        }
+        tex_pos += step;
+        y++;
+    }
+}
+
+void draw_column(t_game *g, t_ray *ray, int x)
+{
+    t_column col;
+    int       ceil_col;
+    int       floor_col;
+
+    init_column(&col, g, ray, x);
+    ceil_col = rgb_to_int(
+        g->data.texture.ceil_rgb[0],
+        g->data.texture.ceil_rgb[1],
+        g->data.texture.ceil_rgb[2]);
+    floor_col = rgb_to_int(
+        g->data.texture.floor_rgb[0],
+        g->data.texture.floor_rgb[1],
+        g->data.texture.floor_rgb[2]);
+    draw_ceiling(g, &col, ceil_col);
+    draw_wall(g, &col);
+    draw_floor(g, &col, floor_col);
 }
