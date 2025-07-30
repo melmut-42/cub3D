040585ca92f5c85b7f539e66e5a59df009527f4c
Melovii <145908254+Melovii@users.noreply.github.com>
2025-07-30 13:03:07 +0300

    feat: centralize mouse while moving

--- srcs/core/events/inputs/mouse.c
+++ srcs/core/events/inputs/mouse.c
@@ -5,30 +5,21 @@ static void	update_rot_angles(t_player *player, int delta_x, int delta_y);
 // * Handles mouse events within the game window
 int handle_mouse(int x, int y, t_game *game)
 {
-    static int  cur_x = NPOS;
-    static int  cur_y = NPOS;
-    int dx;
-    int dy;
+    static int last_x = MID_POINT_X;
+    static int last_y = MID_POINT_Y;
 
-    // Update mouse position for the first time
-    if (cur_x == NPOS || cur_y == NPOS)
+    int dx = x - last_x;
+    int dy = y - last_y;
+
+    if (dx != 0 || dy != 0)
     {
-       cur_x = x;
-       cur_y = y;
-       return (0);
+        update_rot_angles(&game->player, dx, dy);
+        mlx_mouse_move(game->mlx->mlx_ptr, game->mlx->win_ptr, MID_POINT_X, MID_POINT_Y);
     }
-
-    dx = x - cur_x;
-    dy = y - cur_y;
-
-    // update dirs and angles by basing on rotation angles
-    update_rot_angles(&game->player, dx, dy);
-
-    cur_x = x;
-    cur_y = y;
 
     return (0);
 }
+
 
 // * Update rotations and angles by using particular restrictions base on deltas
 static void	update_rot_angles(t_player *player, int delta_x, int delta_y)
