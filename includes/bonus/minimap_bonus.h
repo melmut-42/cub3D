dbe0b2b698963bd76316c969d2770982ea232277
Melovii <145908254+Melovii@users.noreply.github.com>
2025-08-03 13:21:50 +0300

    chore: add missing include for struct

--- includes/bonus/minimap_bonus.h
+++
@@ -1,8 +0,0 @@
-#ifndef MINIMAP_BONUS_H
-# define MINIMAP_BONUS_H
-
-void	draw_minimap(t_game *game);
-void	draw_minimap_rays(t_game *g, t_img *img, t_axis_int offset); 
-
-
-#endif
--- includes/core/bonus.h
+++ includes/core/bonus.h
@@ -5,5 +5,7 @@
 
 void	draw_hud(t_game *game);
 void	mlx_out_txt(void *mlx, void *win, int x, int y, int color, char *str);
+void	draw_minimap(t_game *game);
+void	draw_minimap_rays(t_game *g, t_img *img, t_axis_int offset); 
 
 #endif
